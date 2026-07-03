ef96ced0f0408cb4333bce020e0530911b4f0e2f
Sebastian J. <185572585+0xBash1127@users.noreply.github.com>
2026-07-03 16:20:33 -0400

    Refactor address space calculation and opcode loading

--- Scripts/poppopretFinder.py
+++ Scripts/poppopretFinder.py
@@ -4,15 +4,19 @@ import pykd
 try:
     usage = "Usage: .load pykd\nUsage: !py c:\location\of\script"
 
-    addrSpace = int(sys.argv[2]) - int(sys.argv[1])
-    opcodes = (pykd.loadBytes((int(sys.argv[1])),addrSpace))
+    arg1 = (int(sys.argv[1], base=16))
+    arg2 = (int(sys.argv[2], base=16))
+    addrSpace = int(arg2) - int(arg1)
+    print(addrSpace)
+    opcodes = (pykd.loadBytes(arg1,addrSpace))
+    print(opcodes)
     pop = {0x59,0x5A,0x5D,0x58,0x5E}
     ret = {0xC3}
 
 
     for x in range (len(opcodes) -2):
         if opcodes[x] in pop and opcodes[x+1] in pop and opcodes[x+2] in ret:
-            memlocation = (hex(x + (int(sys.argv[1]))))
+            memlocation = (hex(x + (arg1)))
             print(f"POP POP RET found at:", memlocation)
 
 except IndexError:
